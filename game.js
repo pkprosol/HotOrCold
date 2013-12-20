7da9a3e61380efb26daf06dd316cd8af788cd2bb
Peter Prosol <peter.prosol@gmail.com>
2013-12-20 17:29:59 -0500

    Latest

--- game.js
+++ game.js
@@ -5,16 +5,18 @@ $(document).ready(function() {
 		var userGuess = $('#userGuessInput').val();
 		var random = Math.floor(Math.random()*100);
 		console.log("Original submit works");
-		game(parseInt(userGuess,10), random, "");
+		game(parseInt(userGuess,10), random, "", 1);
 	});
 });
 
 
 
-var game = function(userGuess, random, previousGuess)
+var game = function(userGuess, random, previousGuess, gameCallNumber)
 {
-	console.log(userGuess);
-	console.log(isNaN(previousGuess));
+	console.log("This is call to game function number: " + gameCallNumber);
+	console.log("Current User Guess is: " + userGuess);
+	console.log("Previous Guess = " + previousGuess);
+	console.log("Previous guess is type " + typeof previousGuess);	
 
 	if (userGuess === random) {
 		$('#win').show();	
@@ -33,14 +35,14 @@ var game = function(userGuess, random, previousGuess)
 		$('#colder').show();
 	} 
 
-	console.log(userGuess + " " + random);
+	console.log("Now user guess is: " + userGuess + " and random = " + random);
 
 	var previousGuess = userGuess;
-	$('.messages').hide();
 
 	$('#submit').click(function() {	
+		$('.messages').hide();
 		userGuess = $('#userGuessInput').val();
-		game(parseInt(userGuess,10), random, previousGuess);
+		game(parseInt(userGuess,10), random, parseInt(previousGuess,10), gameCallNumber++);
 	});
 };
 
