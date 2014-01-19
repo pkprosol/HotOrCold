cd798d7380cc3eb0ec23b96f131bb17fc6d2edeb
Peter Prosol <peter.prosol@gmail.com>
2014-01-19 17:26:54 -0500

    Logic check

--- game.js
+++ game.js
@@ -1,49 +1,46 @@
-var gameCallNumber = 0;
-
 $(document).ready(function() {
 	$('.messages').hide();
 
+	var gameCallNumber = 0;
+	var previousGuess = "";
+	var random = Math.floor(Math.random()*100);
+
 	$('#submit').click(function() {
 		var userGuess = $('#userGuessInput').val();
-		var random = Math.floor(Math.random()*100);
-		console.log("Original submit works");
-		game(parseInt(userGuess,10), random, "", gameCallNumber);
+		game(parseInt(userGuess,10), random, previousGuess);
 	});
-});
 
+	var game = function(userGuess, randomNum, previousGuessArg)
+	{
+		console.log("Typeof GameCallNumber: " + typeof gameCallNumber)
+		gameCallNumber = gameCallNumber + 1;
 
+		console.log("This is call to game function number: " + gameCallNumber);
+		console.log("Current User Guess is: " + userGuess);
+		console.log("Previous Guess = " + previousGuess);
+		console.log("Previous guess is type " + typeof previousGuess);
 
-var game = function(userGuess, random, previousGuess, gameCallNumber)
-{
-	console.log("Typeof GameCallNumber: " + typeof gameCallNumber)
-	gameCallNumber = gameCallNumber + 1;
-
-	console.log("This is call to game function number: " + gameCallNumber);
-	console.log("Current User Guess is: " + userGuess);
-	console.log("Previous Guess = " + previousGuess);
-	console.log("Previous guess is type " + typeof previousGuess);
-	console.log("IsNaN(previousGuess) = " + isNaN(previousGuess));	
-
-	if (userGuess === random) {
-		$('#win').show();	
-		return;
-	} else if (previousGuess === "") {
-		if (Math.abs(userGuess - random) < 30) {
-			$('#hot').show();
+		if (userGuess === randomNum) {
+			$('#win').show();	
+			return;
+		} else if (previousGuess === "") {
+			if (Math.abs(userGuess - randomNum) < 30) {
+				$('#hot').show();
+			} else {
+				$('#cold').show();
+			}
+		} else if (Math.abs(userGuess - randomNum) < Math.abs(previousGuess - randomNum)) {
+			$('#hotter').show();
+		} else if (Math.abs(userGuess - randomNum) === Math.abs(previousGuess - randomNum)) {
+			$('#same distance').show();
 		} else {
-			$('#cold').show();
-		}
-	} else if (Math.abs(userGuess - random) < Math.abs(previousGuess - random)) {
-		$('#hotter').show();
-	} else if (Math.abs(userGuess - random) === Math.abs(previousGuess - random)) {
-		$('#same distance').show();
-	} else {
-		$('#colder').show();
-	} 
+			$('#colder').show();
+		} 
 
-	console.log("Now user guess is: " + userGuess + " and random = " + random);
+		console.log("Now user guess is: " + userGuess + " and random = " + randomNum);
 
-	var previousGuess = userGuess;
-};
-
+		previousGuess = parseInt(userGuess,10);
+		console.log("End of statement previous: " + previousGuess);
+	};
+});
 
